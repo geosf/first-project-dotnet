ddcdc3bd0eecebdef2a94cad8e3d6cbb0c392733
Geovani Silva <geovanisilvaf1602@gmail.com>
2021-01-09 02:37:45 -0300

    primeiro projeto dotnet console simples

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using primeiro_projeto.Exemplos;
+
+namespace primeiro_projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Pilha();
+            p.Empilha(1);
+            p.Empilha(10);
+            p.Empilha(100);            
+            Console.WriteLine(p.Desempilha());
+            Console.WriteLine(p.Desempilha());
+            Console.WriteLine(p.Desempilha());
+            Console.WriteLine(p.Desempilha());
+        }
+    }
+}
